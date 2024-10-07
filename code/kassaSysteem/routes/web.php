4bf6a3e532eb2e74ae9f454d1f63454720895467
Jaro Wagener <r0984334@student.thomasmore.be>
2024-10-07 08:43:17 +0200

    Fixen conflict

--- code/kassaSysteem/routes/web.php
+++ code/kassaSysteem/routes/web.php
@@ -21,10 +21,68 @@ Route::get('success', function() {
 
 Route::post('empty-cart', [CartController::class, 'emptyCart'])->name('empty.cart');
 
+
 Route::get('payconic', function() {
     return view('Payconic');
 })->name('payconic');
 
+Route::get('wisselgeldBeheer', function() {
+    $money = [
+        [
+            'id' => 1,
+            'name' => '50euro',
+            'amount' => 50,
+        ],
+        [
+            'id' => 2,
+            'name' => '20euro',
+            'amount' => 20,
+        ],
+        [
+            'id' => 3,
+            'name' => '10euro',
+            'amount' => 10,
+        ],
+        [
+            'id' => 4,
+            'name' => '5euro',
+            'amount' => 5,
+        ],
+        [
+            'id' => 5,
+            'name' => '2euro',
+            'amount' => 2,
+        ],
+        [
+            'id' => 6,
+            'name' => '1euro',
+            'amount' => 1,
+        ],
+        [
+            'id' => 7,
+            'name' => '50cent',
+            'amount' => 50,
+        ],
+        [
+            'id' => 8,
+            'name' => '20cent',
+            'amount' => 20,
+        ],
+        [
+            'id' => 9,
+            'name' => '10cent',
+            'amount' => 10,
+        ],
+        [
+            'id' => 10,
+            'name' => '5cent',
+            'amount' => 5,
+        ],
+    ];
+    return view('wisselgeldBeheer', [
+        'money' => $money,
+    ]);
+});
 
 Route::post('submit', [OrganizationController::class, 'submit'])->name('submit');
 
