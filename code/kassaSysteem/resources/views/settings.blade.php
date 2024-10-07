cb002de4261e4d863889ddcf3fb50c154f6e4046
r0987051 <r0987051@student.thomasmore.be>
2024-10-07 10:19:58 +0200

    Settings basic

--- code/kassaSysteem/resources/views/settings.blade.php
+++ code/kassaSysteem/resources/views/settings.blade.php
@@ -8,10 +8,22 @@
 </head>
 <body class="bg-blue-400 h-screen flex items-center justify-center relative">
 
-<div class="bg-white p-4 rounded-lg shadow-lg">
-    <x-layout.redArrow width="w-80"></x-layout.redArrow>
+<div class="bg-white p-4 rounded-lg shadow-lg w-full max-w-3xl">
+    <div class="flex gap-2">
+        <div class="bg-gray-200 rounded-lg shadow-lg text-center text-2xl font-bold p-4 flex-grow aspect-square flex items-center justify-center">
+            test
+        </div>
+        <div class="bg-gray-200 rounded-lg shadow-lg text-center text-2xl font-bold p-4 flex-grow aspect-square flex items-center justify-center">
+            test
+        </div>
+    </div>
+    <div class="flex gap-2 mt-4">
+        <x-layout.redArrow class="flex-grow" width="w-2/4" />
+        <div class="w-2/4 bg-gray-200 rounded-lg shadow-lg text-center text-2xl font-bold p-4 flex-grow flex items-center justify-center">
+            logout
+        </div>
+    </div>
 </div>
-
 
 </body>
 </html>
